bc303246d40f69e2ff4f6e65acc5271b9384ff9a
plex <plex@onion.localdomain>
2023-08-23 23:04:40 +0300

    task_2

---
+++ 2.Basics/2.2_Packages/task_2.2.1.go
@@ -0,0 +1,8 @@
+package main
+
+// Введите строку кода, чтобы импортировать пакет fmt
+import "fmt"
+
+func main() {
+	fmt.Println("import to import fmt")
+}
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 ## Основы
 ### 2.1 Что такое Go
 1. task_2.1.1 Hello_World!
+### 2.2 Пакеты
+2. task_2.2.1 import fmt
